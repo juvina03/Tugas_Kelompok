c97a35ef0cc733dde659895a6d00dbff490e0189
stefaniechandrax1 <stefaniechandra3@gmail.com>
2021-01-02 21:53:27 +0700

    sc_tambah

---
+++ AndroidStudioProjects/app/src/main/java/com/example/bankqueue/Chooseservice.kt
@@ -0,0 +1,11 @@
+package com.example.bankqueue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Chooseservice : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chooseservice)
+    }
+}
---
+++ AndroidStudioProjects/app/src/main/java/com/example/bankqueue/Employeepart.kt
@@ -0,0 +1,11 @@
+package com.example.bankqueue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Employeepart : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_employeepart)
+    }
+}
